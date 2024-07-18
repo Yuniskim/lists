37338b0c2675a112aabf11f9bc898d25e452eb59
yuniskim <yuniskimani2015@gmail.com>
2024-07-18 03:52:20 -0700

    python lists

---
+++ lists.py
@@ -0,0 +1,25 @@
+my_list = []
+print(my_list)
+#append elements
+my_list = []
+my_list.append(10)
+print(my_list) 
+my_list.append(20)
+print(my_list)
+my_list.append(30)
+print(my_list)
+my_list.append(40)
+print(my_list)
+my_list = [10,20,30,40,50]
+my_list.insert(2,15)  
+print(my_list)
+add_list = [50,60,70]
+my_list.extend(add_list)
+print(my_list)
+del my_list[-1]
+print(my_list)
+my_list = [10,20,15,30,40,50,50,60,70]
+sorted_list = sorted(my_list)
+print(sorted_list)
+index_of_30 = my_list.index(30)
+print("Index of value 30:", index_of_30) 
